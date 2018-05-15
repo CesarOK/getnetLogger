a760867b3fbdce596aa8f35802ee356ca464c527
CesarOK <you@example.com>
2018-05-14 21:18:47 -0300

    new name and class

--- Logger.py
+++ Logger.py
@@ -4,8 +4,6 @@
 import sched, time, base64, zlib, json
 from StringIO import StringIO
 
-
-
 ClientID = "45bd882c-8321-42e1-ac53-feb8aaf071ea"
 ClientSecret = "ff491be0-fee7-4890-8c6a-7df85ac1a41e"
 encodedB64 = base64.b64encode(ClientID + ':' + ClientSecret)
@@ -13,8 +11,8 @@ encodedB64 = base64.b64encode(ClientID + ':' + ClientSecret)
 s = sched.scheduler(time.time, time.sleep)
 def touchGetnet(sc): 
 	print "Doing stuff..."
-	# authorize()
-	# touchService()
+	# getnet.authenticate()
+	# getnet.touch(microService)
 	s.enter(5, 1, touchGetnet, (sc,))
 
 s.enter(5, 1, touchGetnet, (s,))
